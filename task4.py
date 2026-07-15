e969e132eac344a19e95d1a67c68d887fbca92ee
KanzuNe <hieu04092007@gmail.com>
2026-07-15 23:41:22 +0700

    hoàn thiện nốt pipeline RAG cơ bản

--- task4.py
+++ task4.py
@@ -1,7 +1,15 @@
+from langchain_classic.chains.retrieval import create_retrieval_chain
 from langchain_community.document_loaders import TextLoader
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+import os
+from dotenv import load_dotenv
+load_dotenv()
+key = os.getenv("GEMINI_API_KEY")
 
 loader = TextLoader(r"D:\CODING\RAG Test\quy_che.txt", encoding="utf-8")
 documents = loader.load()
@@ -13,6 +21,15 @@ embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2"
 
 db = Chroma.from_documents(docs, embedding_function)
 
-res = db.similarity_search(query="Tôi không muốn bị đuổi khỏi phòng thi", k=1)
 
-print(res)
+os.environ["GOOGLE_API_KEY"] = key
+llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
+ai_msg = llm.invoke("hello")
+chat_template = ChatPromptTemplate.from_messages([
+    ("Bạn là một trợ lí ảo của PTIT, hãy trả lời câu hỏi dựa vào context sau đây:{context}, câu hỏi là{input}")
+])
+document_chain = create_stuff_documents_chain(llm=llm,prompt=chat_template)
+retrieve = db.as_retriever()
+retrivial_chain = create_retrieval_chain(retrieve,document_chain)
+response = retrivial_chain.invoke({"input": "Tôi không muốn bị đuổi khỏi phòng thi thì sao?"})
+print(response)
